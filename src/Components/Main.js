84ee5f39628ec8419bd2b9c9f5a189cf22aaadbd
alberto leandro sanchez <beto.unahur@gmail.com>
2021-07-27 18:58:36 -0300

    .

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,35 +1,10 @@
-/*import React from 'react'
-import { Thickness, LevelAnnotationConfig, AnnotationType, Colors, LineType, GroupByType, PageFitMode, Enabled } from 'basicprimitives';
-import {OrgDiagram} from 'basicprimitivesreact'
-import config from '../helpers/config'
-import styled from 'styled-components';
-
-const MainDiagram = styled.div`
-    width: 100vw;
-    height: 100vh;
-`
-
-function Main() {
-
-    
-
-    return (
-        <MainDiagram>
-            <OrgDiagram centerOnCursor={true} config={config}>
-
-            </OrgDiagram>
-        </MainDiagram>
-
-    )
-}
-
-export default Main
-*/
-import React, { Component } from 'react';
-import { OrgDiagram, FamDiagram } from 'basicprimitivesreact';
-import { Thickness, LevelAnnotationConfig, AnnotationType, Colors, LineType, GroupByType, PageFitMode, Enabled } from 'basicprimitives';
+import React from 'react';
+import {  FamDiagram } from 'basicprimitivesreact';
 import {config} from '../helpers/config';
 import styled from 'styled-components';
+import { Button } from 'react-bootstrap';
+
+
 
 const MainDiagram = styled.div`
     width: 100vw;
@@ -39,7 +14,9 @@ const MainDiagram = styled.div`
   function Main() {
  
     return <MainDiagram>
-      
+        <Button
+        style={{ display: 'inline',position:'fixed', margin: '0px', zIndex: '1000', top: '0px', left: '0px' }}
+        href="https://github.com/albertoleandrosanchez/diagrama"><i class="bi bi-github">  Repositorio</i></Button>
         <FamDiagram style={{backgroundImage: 
             'red'
             //'linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%)'
